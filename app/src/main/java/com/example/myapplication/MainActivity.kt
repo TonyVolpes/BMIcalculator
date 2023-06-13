1d57b2fe2a1f6e545cecc73b93e95e85f79d20bb
TonyVolpes <127159861+TonyVolpes@users.noreply.github.com>
2023-06-13 13:40:13 +0200

    MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -17,29 +17,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Collegamento delle variabili di istanza alle viste nel layout
+      
         editHeight = findViewById(R.id.editHeight)
         editWeight = findViewById(R.id.editWeight)
         btnCalculate = findViewById(R.id.btnCalculate)
         textResult = findViewById(R.id.textResult)
 
-        // Impostazione di un listener per il pulsante di calcolo
+     
         btnCalculate.setOnClickListener {
             calculateBMI()
         }
     }
 
     private fun calculateBMI() {
-        // Recupero dei valori di altezza e peso inseriti dall'utente
+       
         val height = editHeight.text.toString().toFloatOrNull()
         val weight = editWeight.text.toString().toFloatOrNull()
 
-        // Controllo se gli input sono validi
+        
         if (height != null && weight != null) {
-            // Calcolo del BMI utilizzando la formula peso / (altezza^2)
+       
             val bmi = weight / height.pow(2)
 
-            // Determinazione della condizione corrispondente al valore del BMI
+            
             val resultText = when {
                 bmi < 18.5 -> "Sottopeso"
                 bmi < 25 -> "Normale"
@@ -47,10 +47,10 @@ class MainActivity : AppCompatActivity() {
                 else -> "Obeso"
             }
 
-            // Impostazione del risultato nella vista textResult utilizzando la risorsa di stringa bmi_result
+         
             textResult.setText(getString(R.string.bmi_result, bmi, resultText))
         } else {
-            // Visualizzazione di un messaggio di errore se gli input non sono validi
+     
             textResult.text = "Inserisci valori validi per altezza e peso."
         }
     }
